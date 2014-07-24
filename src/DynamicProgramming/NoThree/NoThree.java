21c1b3ad501b98094fd4174724b1e34a32cb8283
ROB WOOD <poker199101@gmail.com>
2014-07-23 20:50:29 -0400

    Initial commit

---
+++ src/DynamicProgramming/NoThree/NoThree.java
@@ -0,0 +1,19 @@
+/**
+ * Filename:   NoThree.java
+ * Classname:  NoThree
+ * Version:    1.0
+ * Date:       7/23/2014
+ * Created by: Umang Gala
+ */
+package DynamicProgramming.NoThree;
+
+/**
+ * Description:
+ *
+ * @author Umang Gala
+ */
+public class NoThree {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
