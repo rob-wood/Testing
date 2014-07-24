822fa8cd3204e102bdda35fe3755d03a87877617
ROB WOOD <poker199101@gmail.com>
2014-07-23 20:59:33 -0400

    Initial commit

--- src/DynamicProgramming/NoThree/NoThree.java
+++ src/DynamicProgramming/NoThree/NoThree.java
@@ -15,6 +15,5 @@ package DynamicProgramming.NoThree;
 public class NoThree {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Git");
     }
 }
